22a610dbe24d974e248a640c9f89666c8d139850
Charlie Blackstock <charlieblackstock@gmail.com>
2016-03-15 17:08:31 -0600

    finished testing for reducers and actions

--- src/components/comment_box.jsx
+++ src/components/comment_box.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 export default class CommentBox extends Component {
     constructor(props) {
@@ -14,6 +16,7 @@ export default class CommentBox extends Component {
     handleSubmit(event) {
         event.preventDefault();
 
+        this.props.saveComment(this.state.comment);
         this.setState({ comment: '' });
     }
 
@@ -22,12 +25,16 @@ export default class CommentBox extends Component {
             <form
                 onSubmit={this.handleSubmit.bind(this)}
                 className="comment-box">
+                <h4>Add a Comment</h4>
                 <textarea
                     value={this.state.comment}
                     onChange={this.handleChange.bind(this)}
                 />
+                <br/>
                 <button action="submit">Submit Comment</button>
             </form>
         )
     }
 }
+
+export default connect(null, actions)(CommentBox);
